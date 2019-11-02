53904dd7155368b0ebc6bcf1ed05d0033959a76d
nabiloo19 <nabil.alanssi19@gmail.com>
2019-11-02 17:54:47 +0300

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDtOwdB_lx2yZbYJ8eRczvy_3-1UuTL_rg",
+    authDomain: "vote-98609.firebaseapp.com",
+    databaseURL: "https://vote-98609.firebaseio.com",
+    projectId: "vote-98609",
+    storageBucket: "vote-98609.appspot.com",
+    messagingSenderId: "1045533540053",
+    appId: "1:1045533540053:web:6ad7ae6cfbd47b01dd6c5c"
+  };
+
+  firebase.initializeApp(firebaseConfig);
